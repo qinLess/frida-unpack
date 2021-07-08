ecfd4c5497f87a8fac2ad62af5b8e1e812025be3
admin <admin@admindeMacBook-Pro-4.local>
2021-07-08 14:36:07 +0800

    update

--- frida_unpack.py
+++ frida_unpack.py
@@ -1,17 +1,19 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # coding=utf-8
 import frida
 import sys
 
+
 def on_message(message, data):
     base = message['payload']['base']
     size = int(message['payload']['size'])
-    print(hex(base),size)
+    print(hex(base), size)
     # print session
     # dex_bytes = session.read_bytes(base, size)
     # f = open("1.dex","wb")
     # f.write(dex_bytes)
     # f.close()
+
 
 # 9.0 arm 需要拦截　_ZN3art13DexFileLoader10OpenCommonEPKhjS2_jRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE
 # 7.0 arm：_ZN3art7DexFile10OpenMemoryEPKhjRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPNS_6MemMapEPKNS_10OatDexFileEPS9_
@@ -20,7 +22,7 @@ def on_message(message, data):
 # #_ZN3art13DexFileLoader10OpenCommonEPKhjS2_jRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE
 
 package = sys.argv[1]
-print("dex 导出目录为: /data/data/%s"%(package))
+print("dex 导出目录为: /data/data/%s" % (package))
 device = frida.get_usb_device()
 pid = device.spawn(package)
 session = device.attach(pid)
@@ -53,11 +55,11 @@ Interceptor.attach(Module.findExportByName("libdexfile.so", "_ZN3art13DexFileLoa
         }
     }
 });
-"""%(package)
+""" % (package)
 
 script = session.create_script(src)
 
-script.on("message" , on_message)
+script.on("message", on_message)
 
 script.load()
 device.resume(pid)
