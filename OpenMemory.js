3f1d9627c51b25280cce95fc385d619d2eb93caa
admin <admin@admindeMacBook-Pro-11.local>
2021-10-18 17:29:51 +0800

    update

--- OpenMemory.js
+++ OpenMemory.js
@@ -6,24 +6,21 @@
  * 360:libjiagu.so
  */
 
+// frida -U -f com.yaya.zone -l OpenMemory.js --no-pause
+
 // android 10
-// var symbolAndroid = '_ZN3art16ArtDexFileLoader10OpenCommonEPKhmS2_mRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS_13DexFileLoader12VerifyResultE'
-// var soName = 'libdexfile.so'
+var symbolAndroid = '_ZN3art13DexFileLoader10OpenCommonEPKhjS2_jRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE'
+var soName = 'libdexfile.so'
 
 // android 8.1.0
-var symbolAndroid = '_ZN3art7DexFile10OpenCommonEPKhmRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_PNS0_12VerifyResultE';
-var soName = 'libart.so';
+// var symbolAndroid = '_ZN3art7DexFile10OpenCommonEPKhmRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_PNS0_12VerifyResultE';
+// var soName = 'libart.so';
 
 var soFunction = Module.findExportByName(soName, symbolAndroid);
 console.log('soFunction: ', soFunction)
 
 Interceptor.attach(soFunction, {
     onEnter: function (args) {
-        console.log('args[0]: ', Memory.readUtf8String(args[0]));
-        console.log('args[2]: ', Memory.readUtf8String(args[2]));
-        console.log('args[3]: ', Memory.readUtf8String(args[3]));
-        console.log('args[1]: ', Memory.readUtf8String(args[1]));
-
         //dex起始位置
         var begin = args[1]
         //打印magic
@@ -35,7 +32,8 @@ Interceptor.attach(soFunction, {
 
         console.log('dex_size: ', dex_size)
         //dump dex 到/data/data/pkg/目录下
-        var file = new File('/com.wanwu.wanwu/' + dex_size + '.dex', 'wb')
+        var file = new File('/sdcard/dingdong/' + dex_size + '.dex', 'wb')
+        // var file = new File('/data/data/com.yaya.zone/' + dex_size + '.dex', 'wb')
         file.write(Memory.readByteArray(begin, dex_size))
         file.flush()
         file.close()
